602c4898fba38c10f6398a5486ba2f934bbd2cd8
MNightfall <madlencrazy@gmail.com>
2024-09-23 16:00:10 +0100

    Task 2

---
+++ Task 1 hello_world.py
@@ -0,0 +1,14 @@
+# 1. Prompt the user to enter their name.
+name = input("Please enter your name: ")
+
+# 2. Print out the user's name.
+print("Your name is:", name)
+
+# 3. Prompt the user to enter their age.
+age = input("Please enter your age: ")
+
+# 4. Print out the user's age.
+print("Your age is:", age)
+
+# 5. Print "Hello World!" on a new line.
+print("Hello World!")
---
+++ Task 2 Conversion.py
@@ -0,0 +1,23 @@
+# 1. Declare the variables
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+# 2. Convert num1 into an integer
+num1_int = int(num1)
+
+# 3. Convert num2 into a float
+num2_float = float(num2)
+
+# 4. Convert num3 into a string
+num3_str = str(num3)
+
+# 5. Convert string1 into an integer
+string1_int = int(string1)
+
+# 6. Print each converted variable on a new line
+print(num1_int)     # Printing integer version of num1
+print(num2_float)   # Printing float version of num2
+print(num3_str)     # Printing string version of num3
+print(string1_int)  # Printing integer version of string1
